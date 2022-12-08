9b41500d5e7f6c920fdf0b73627e0774fa191509
bmn44100 <njerumurimi@gmail.com>
2022-12-08 10:28:57 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 
 /*
  *
- *main - print the size of various types
+ *main - print the size of various types on the computer it is compiled on
  *Description: size of various types on the computer it is compiled and run on
  *
  *Return: Always 0 (Success)
