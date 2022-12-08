f2287c6d4c90e75db7bc6d65800ed6ad8c8d0fc6
bmn44100 <njerumurimi@gmail.com>
2022-12-08 06:23:41 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
 int main(void)
 
 {
