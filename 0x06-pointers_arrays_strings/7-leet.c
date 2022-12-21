f4838c9bea27ed77f034692d6b11ec427cf356b5
bmn44100 <njerumurimi@gmail.com>
2022-12-21 07:27:28 -0800

    function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,13 +14,13 @@ char *leet(char *s)
 	{
 		if (s[i] == 'a' || s[i] == 'A')
 			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
+		else if (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
+		else if (s[i] == 'o' || s[i] == 'O')
 			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
+		else if (s[i] == 't' || s[i] == 'T')
 			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
+		else (s[i] == 'l' || s[i] == 'L')
 			s[i] = '1';
 	}
 
