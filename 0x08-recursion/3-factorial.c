e996c512ba5e69a3dc333fa6f526c8f73d74eaa7
bmn44100 <njerumurimi@gmail.com>
2023-01-05 10:38:36 +0000

    function that returns the value of x raised to the power of y.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * factorial - factorial of a given number
+ * @n: integer
+ * Return: integer factorial
+ */
+
+int factorial(int n)
+{
+
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
