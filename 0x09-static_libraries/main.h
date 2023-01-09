fc025796920d4952542307db1f39a96b15a04ff4
bmn44100 <njerumurimi@gmail.com>
2023-01-09 03:06:38 -0800

    static library libmy.a containing all the functions listed below

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -8,11 +8,6 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
