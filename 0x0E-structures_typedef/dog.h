d98e57437796d6cf3d4e513b49fc455cff509bc7
bmn44100 <njerumurimi@gmail.com>
2023-01-17 01:53:51 -0800

    type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,11 @@
-#ifndef DOG
-#define DOG
-
 /**
  * struct dog - describing a dog
+ *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Description: descibing a dog
+ * Description: I am descibing a dog
  */
 
 struct dog
