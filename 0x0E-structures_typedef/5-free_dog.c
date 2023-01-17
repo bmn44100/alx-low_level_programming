d50f54b02bd28a345f32a8fc54290f0dbc9d30b5
bmn44100 <njerumurimi@gmail.com>
2023-01-17 03:53:53 -0800

    a function that frees memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to a structure
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
