bf78ab73db8dea007f6727437f553a753e73bf25
bmn44100 <njerumurimi@gmail.com>
2023-01-30 03:10:09 -0800

    function that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * premain - prints before the main function is executed.
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
