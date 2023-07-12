9bae078244921c5ed0c20f5713ccd580b53afb71
bmn44100 <njerumurimi@gmail.com>
2023-07-12 15:03:13 -0700

    function that searches for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,11 +25,13 @@ int search_array(int *array, size_t size, size_t first, size_t last, int val)
 }
 
 /**
- * jump_search - function that searches for a value in a sorted array of integers using the Jump search algorithm
+ * jump_search - function that searches for a value in a sorted array
+ * of integers using the Jump search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: the value to search for
- * Return: the first index where value is located or -1 value is not present in array or if array is NULL
+ * Return: the first index where value is located or -1 value is
+ * not present in array or if array is NULL
  */
 
 int jump_search(int *array, size_t size, int value)
